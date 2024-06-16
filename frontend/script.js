92a1cb403f8ca22db3d0726724d56818b3ae4694
apjanusz <januszekaptacy2606@gmail.com>
2024-06-16 21:23:30 +0200

    functionality works

--- frontend/script.js
+++ frontend/script.js
@@ -10,56 +10,72 @@ function fetchData() {
       measurements = res;
       selection();
       selection2();
+      table();
     })
     .catch((error) => console.log("Błąd: ", error));
 }
 
-
-// adding new measurements to json server 
+// adding new measurements to json server
 function addData() {
   const weight = parseFloat(document.getElementById("weight").value);
-  const height = parseFloat(document.getElementById("height").value);
-  const kcal = parseFloat(document.getElementById("kcal").value);
-  id=`${parseInt(measurements[measurements.length-1].id) + 1}`;
+  const height = parseInt(document.getElementById("height").value);
+  const kcal = parseInt(document.getElementById("kcal").value);
+  id = `${parseInt(measurements[measurements.length - 1].id) + 1}`;
   fetch(SERVER, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({id, weight, height, kcal}),
+    body: JSON.stringify({ id, weight, height, kcal }),
   })
-    .then((res) => res.json())
     .then((res) => {
+      if (res.ok) {
+        fetchData();
+        alert(`Pomyslnie dodano dane.`);
+      } else {
+        return res.text().then((text) => {
+          throw new Error(text);
+        });
+      }
       console.log(res);
     })
-    .catch((error) => console.log("Błąd: ", error));
+    .catch((error) =>
+      alert(
+        `Problem podczas dodawania danych o nastepujacym numerze id: ${id} \nOdpowiedz serwera: \n${error}`
+      )
+    );
 }
 
 // remove measurements from json server
 function removeData() {
-  const id = document.getElementById('mySelect').value;
+  const id = document.getElementById("mySelect").value;
 
   fetch(`${SERVER}/${id}`, {
     method: "DELETE",
-    })
+  })
     .then((res) => {
       if (res.ok) {
         fetchData();
         alert(`Pomyslnie usunieto dane o nastepujacym numerze id: ${id}`);
       } else {
-        return res.text().then(text => {throw new Error(text)});
+        return res.text().then((text) => {
+          throw new Error(text);
+        });
       }
       console.log(res);
     })
-    .catch((error) => alert(`Problem podczas usuwania danych o nastepujacym numerze id: ${id} \nOdpowiedz serwera: \n${error}`));
-
+    .catch((error) =>
+      alert(
+        `Problem podczas usuwania danych o nastepujacym numerze id: ${id} \nOdpowiedz serwera: \n${error}`
+      )
+    );
 }
 
 // edit measurements from json server
 function updateData() {
-  const id = document.getElementById('mySelect2').value;
-  
+  const id = document.getElementById("mySelect2").value;
+
   for (var i = 0; i < measurements.length; i++) {
     if (id === measurements[i].id) {
       var weight = measurements[i].weight;
@@ -67,10 +83,10 @@ function updateData() {
       var kcal = measurements[i].kcal;
     }
   }
-  const weightEd = document.getElementById('weight-edit').value;
-  const heightEd = document.getElementById('height-edit').value;
-  const kcalEd = document.getElementById('kcal-edit').value;
-  
+  const weightEd = document.getElementById("weight-edit").value;
+  const heightEd = document.getElementById("height-edit").value;
+  const kcalEd = document.getElementById("kcal-edit").value;
+
   if (weightEd !== null && weightEd !== "") {
     weight = parseFloat(weightEd);
   }
@@ -87,56 +103,107 @@ function updateData() {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ id, weight, height, kcal}),
+    body: JSON.stringify({ id, weight, height, kcal }),
   })
-    .then((res) => res.json())
     .then((res) => {
+      if (res.ok) {
+        fetchData();
+        alert(`Pomyslnie zedytowano dane o nastepujacym numerze id: ${id}`);
+      } else {
+        return res.text().then((text) => {
+          throw new Error(text);
+        });
+      }
       console.log(res);
-      console.log(measurements.length);
     })
-    .catch((error) => console.log("Błąd: ", error));
-
+    .catch((error) =>
+      alert(
+        `Problem podczas edytowania danych o nastepujacym numerze id: ${id} \nOdpowiedz serwera: \n${error}`
+      )
+    );
 }
 
-
-// create select
+// create selection
 function selection() {
   var myDiv = document.getElementById("myDiv");
-
-  //Create and append select list
+  myDiv.innerHTML = "";
   var selectList = document.createElement("select");
   selectList.setAttribute("id", "mySelect");
-  myDiv.innerHTML = '';
   myDiv.appendChild(selectList);
-  
-  //Create and append the options
+
   for (var i = 0; i < measurements.length; i++) {
-      var option = document.createElement("option");
-      option.setAttribute("value", measurements[i].id);
-      option.text = measurements[i].id;
-      selectList.appendChild(option);
+    var option = document.createElement("option");
+    option.setAttribute("value", measurements[i].id);
+    option.text = measurements[i].id;
+    selectList.appendChild(option);
   }
 }
 
 //create second selection
 function selection2() {
   var myDiv2 = document.getElementById("myDiv2");
-
-  //Create and append select list
+  myDiv2.innerHTML = "";
   var selectList2 = document.createElement("select");
   selectList2.setAttribute("id", "mySelect2");
-  myDiv2.innerHTML = '';
   myDiv2.appendChild(selectList2);
-  
-  //Create and append the options
+
   for (var i = 0; i < measurements.length; i++) {
-      var option2 = document.createElement("option");
-      option2.setAttribute("value", measurements[i].id);
-      option2.text = measurements[i].id;
-      selectList2.appendChild(option2);
+    var option2 = document.createElement("option");
+    option2.setAttribute("value", measurements[i].id);
+    option2.text = measurements[i].id;
+    selectList2.appendChild(option2);
   }
 }
 
+//create a table
+function table() {
+  var table = document.getElementById("myTable");
+
+  for (var i = 0; i < measurements.length; i++) {
+    var row = table.insertRow(i + 1);
+    var celId = row.insertCell(0);
+    var celWeight = row.insertCell(1);
+    var celHeight = row.insertCell(2);
+    var celBMI = row.insertCell(3);
+    var celKcal = row.insertCell(4);
+
+    celId.innerHTML = measurements[i].id;
+    celWeight.innerHTML = measurements[i].weight;
+    celHeight.innerHTML = measurements[i].height;
+    celKcal.innerHTML = measurements[i].kcal;
+    h = (measurements[i].height / 100) * (measurements[i].height / 100);
+    bmi = (measurements[i].weight / h).toFixed(2);
+    celBMI.innerHTML = bmi;
+  }
+}
+// sort a table
+function sortTable(index) {
+  let table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("myTable");
+  switching = true;
+
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("td")[index];
+      y = rows[i + 1].getElementsByTagName("td")[index];
+
+      if (parseFloat(x.innerHTML) > parseFloat(y.innerHTML)) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
+
+
 fetchData();
-//document.addEventListener("DOMContentLoaded", selection);
-//updateData();
